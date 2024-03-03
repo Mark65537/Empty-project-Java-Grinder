96c5b89ea8f2be25bf05af0fe244a3a83e3f3f6f
Mark65537 <Mark65537@gmail.com>
2024-03-03 22:46:07 +0700

    new: добавление игрового цикла

--- src/Main.java
+++ src/Main.java
@@ -34,8 +34,85 @@ public class Main
 
     // PlayBackgroundMusic();
     
+    // Game loop
+    while(isPlayed) {        
 
-    
+      keyCode = SegaGenesis.getJoypadValuePort1();
+      
+      //проверка нажатий кнопок
+      if(!pressed){
+        for (int i = 0x0000; i <= 0xF000; i+=0x0100) {
+              // проверка нажатия кнопки вверх
+              if(keyCode == (i+0x0081) && y > 0x7F) {                      
+                SegaGenesis.setSpritePosition(0, x, y -= speed);
+                break;
+              }
+              
+              // проверка нажатия кнопки вниз            
+              if(keyCode == (i+0x0082) && y < 0x160) {                      
+                SegaGenesis.setSpritePosition(0, x, y += speed);
+                break;
+              }
+              
+              // проверка нажатия кнопки влево                    
+              if(keyCode == (i+0x0084) && x > 0x7E) {                      
+                SegaGenesis.setSpritePosition(0, x -= speed, y);
+                break;                
+              }
+
+              // проверка нажатия кнопки вправо          
+              if(keyCode == (i+0x0088) && x < 0x1C0) {                      
+                SegaGenesis.setSpritePosition(0, x += speed, y);
+                break;
+              }
+
+              // проверка нажатия кнопки A         
+              if(keyCode == (i+0xD080)) {
+
+                pressed = true;
+                break;
+              }
+
+              // проверка нажатия кнопки B          
+              if(keyCode == (i+0x0090)) {
+
+                SprArrow.setTo0();
+                          
+                pressed = true;
+                break;
+              }
+
+              // проверка нажатия кнопки C          
+              if(keyCode == (i+0x00A0)) {                      
+
+                pressed = true;
+                break;
+              }
+
+              // проверка нажатия кнопки START          
+              if(keyCode == (i+0xE080)) {
+                if(state == GameState.LOGO) {
+                                   
+                  state = GameState.INFO;
+                }
+                else if (state == GameState.INFO) {
+                  
+                  state = GameState.INGAME;
+                }
+                else if (state == GameState.GAMEOVER) {                  
+
+                  state = GameState.LOGO;
+                }
+                pressed = true;
+                break;
+              }
+        }          
+      }
+      else if(keyCode == 0xCC80 || keyCode == 0xC080) {
+        pressed = false;
+      }        
+    }
+  
   }
 
   
